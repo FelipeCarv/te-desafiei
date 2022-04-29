26925dd8ba535ad4c9607b2bef8bd52cdf68caec
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-04-29 16:21:57 -0300

    adicionando counter up, efeitos fade e iniciando o responsivo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 import Dom from './modules/constructors.js'; //selecionar elementos
 import scriptsHome from './modules/scriptsPaginas/home.js'
-
+import initCounterUp from './modules/counterUp.js';
 
 // PAGES 
 const pageHome = new Dom().el("#page__home")
@@ -9,6 +9,7 @@ const pageHome = new Dom().el("#page__home")
 if (pageHome) { // ★ HOME  
     new Dom().bodyClass("body__home");
     scriptsHome();
+    initCounterUp();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
