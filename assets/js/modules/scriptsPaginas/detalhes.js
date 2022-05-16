40eb1168e0b2dcf65d2abd4d90119b3d3d1c3509
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2022-05-16 15:57:44 -0300

    adicionando modais da página detalhes

--- assets/js/modules/scriptsPaginas/detalhes.js
+++ assets/js/modules/scriptsPaginas/detalhes.js
@@ -1,3 +1,54 @@
 export default function scriptsDetalhes(){
     // 'colocar scripts das respectivas paginas dentro de -scriptsPaginas-  e criar o arquivo exemplo: home.js - contato.js  --- e importar no main e executar em sua respectiva pagina '
+
+    // ============== MODAL PLANOS ==============
+
+    const btnAlterar = document.getElementById('alterar');
+    const btnFechar = document.getElementById('fechar')
+
+    btnAlterar.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const modal = document.getElementById('modal-planos');
+        modal.classList.add('mostrar');
+    });
+
+    btnFechar.addEventListener('click', ()=>{
+        const modal = document.getElementById('modal-planos');
+        modal.classList.remove('mostrar');
+    });
+
+
+    // ============== MODAL CANCELAR ==============
+
+    const btnCancelar = document.getElementById('cancelar');
+    const btnVoltarCancelar = document.getElementById('voltar-cancelar');
+    const btnCancelarModal = document.getElementById('btnCancelar');
+
+    function fecharModal(e){
+        e.preventDefault();
+        const modal = document.getElementById('modal-cancelar');
+        modal.classList.remove('mostrar');
+    }
+
+    btnCancelar.addEventListener('click', (e)=>{
+        e.preventDefault();
+        const modal = document.getElementById('modal-cancelar');
+        modal.classList.add('mostrar');
+    });
+
+    btnVoltarCancelar.addEventListener('click', fecharModal);
+    btnCancelarModal.addEventListener('click', fecharModal);
+
+
+
+    // ======== MODAL PLANO CANCELADO ========
+    const btnCancelado = document.getElementById('quero-cancelar');
+
+    btnCancelado.addEventListener('click', (e)=>{
+        
+        const modal = document.getElementById('modal-cancelado');
+        modal.classList.add('mostrar');
+        
+    });
+    e.preventDefault();
 }
